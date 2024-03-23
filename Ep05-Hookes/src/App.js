c37bf079d3d6caa7563f002075ee56a5f15d986a
Kasifmansuri7 <kasif.mansuri.7@gmail.com>
2024-03-23 14:04:48 +0530

    update

--- Ep05-Hookes/src/App.js
+++ Ep05-Hookes/src/App.js
@@ -1,27 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import { restaurantsData } from "./utils/mockData";
 import RestaurantsCard from "./components/RestaurantsCard";
 import "./App.css";
 
 function App() {
-  let [kashif, setKashif] = useState(restaurantsData);
+  let [restaurantList, setRestaurantList] = useState(restaurantsData);
 
-  function handleFilter() {
-    const filteredRes = kashif.filter((each) => each.info.avgRating > 4.2);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    setKashif(filteredRes);
-  }
+  const fetchData = async () => {
+    const res = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.02760&lng=72.58710&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const data = await res.json();
+    setRestaurantList(
+      data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
 
   return (
     <>
       <Header />
 
-      <button className="filter-btn" onClick={handleFilter}>
+      <button
+        className="filter-btn"
+        onClick={() =>
+          setRestaurantList((prev) =>
+            prev.filter((each) => each.info.avgRating > 4.2)
+          )
+        }
+      >
         Top Rated Restaurants
       </button>
       <section className="res-container">
-        {kashif.map((res) => (
+        {restaurantList.map((res) => (
           <RestaurantsCard key={res.info.id} {...res.info} />
         ))}
       </section>
